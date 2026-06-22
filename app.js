91bc4498b5b3c41f16289a7e396c007c809d4edc
Thogari  Yashwant <yashwanthroyals1@gmail.com>
2026-06-22 14:03:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,238 @@
+import { db, collection, addDoc, getDocs } from "./firebase.js";
+
+console.log("Firebase Connected");
+console.log(db);
+
+// Modal Elements
+const addJobBtn = document.getElementById("addJobBtn");
+const jobModal = document.getElementById("jobModal");
+const closeModal = document.getElementById("closeModal");
+const totalJobsElement = document.getElementById("totalJobs");
+const appliedJobsElement = document.getElementById("appliedJobs");
+const pendingJobsElement = document.getElementById("pendingJobs");
+const viewModal = document.getElementById("viewModal");
+const closeViewModal = document.getElementById("closeViewModal");
+const jobDetails = document.getElementById("jobDetails");
+// Form Elements
+const jobForm = document.getElementById("jobForm");
+const companyInput = document.getElementById("company");
+const roleInput = document.getElementById("role");
+const jobTableBody = document.getElementById("jobTableBody");
+const jobLinkInput = document.getElementById("jobLink");
+const addedByInput = document.getElementById("addedBy");
+const statusInput = document.getElementById("status");
+const notesInput = document.getElementById("notes");
+const resumeInput = document.getElementById("resume");
+const saveJobBtn = document.getElementById("saveJobBtn");
+// Jobs Array
+
+let jobs = JSON.parse(localStorage.getItem("jobs")) || [];
+const jobsCollection = collection(db, "jobs");
+async function saveJobToFirebase(job) {
+  try {
+    await addDoc(jobsCollection, job);
+
+    console.log("Job saved to Firebase");
+  } catch (error) {
+    console.error("Firebase Error:", error);
+  }
+}
+// Open Modal
+addJobBtn.addEventListener("click", () => {
+  jobModal.classList.remove("hidden");
+  jobModal.classList.add("flex");
+});
+
+// Close Modal
+closeModal.addEventListener("click", () => {
+  jobModal.classList.add("hidden");
+  jobModal.classList.remove("flex");
+});
+
+// Form Submit
+jobForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const company = companyInput.value.trim();
+  const role = roleInput.value.trim();
+  const jobLink = jobLinkInput.value.trim();
+  const addedBy = addedByInput.value;
+  const status = statusInput.value;
+  const notes = notesInput.value.trim();
+  const resume = resumeInput.files[0];
+  if (!company || !role) {
+    alert("Please fill all fields");
+    return;
+  }
+  const job = {
+    company,
+    role,
+    jobLink,
+    addedBy,
+    status,
+    notes,
+    resumeName: resume ? resume.name : "No Resume",
+    resumeUrl: resume ? URL.createObjectURL(resume) : "",
+  };
+  if (editIndex !== null) {
+    jobs[editIndex] = job;
+
+    editIndex = null;
+
+    saveJobBtn.textContent = "Save Job";
+  } else {
+    jobs.push(job);
+    saveJobToFirebase(job);
+  }
+  localStorage.setItem("jobs", JSON.stringify(jobs));
+  console.log(jobs);
+  renderJobs();
+  updateDashboard();
+  saveToLocalStorage();
+  jobForm.reset();
+  jobModal.classList.add("hidden");
+  jobModal.classList.remove("flex");
+});
+closeViewModal.addEventListener("click", () => {
+  viewModal.classList.add("hidden");
+  viewModal.classList.remove("flex");
+});
+// Render Jobs
+function renderJobs() {
+  jobTableBody.innerHTML = "";
+  jobs.forEach((job, index) => {
+    const row = document.createElement("tr");
+    row.className = "border-t";
+    row.innerHTML = `
+  <td class="px-6 py-4">${job.company}</td>
+  <td class="px-6 py-4">${job.role}</td>
+  <td class="px-6 py-4">${job.addedBy}</td>
+<td class="px-6 py-4">
+  ${
+    job.resumeUrl
+      ? `<a href="${job.resumeUrl}" target="_blank"
+           class="text-blue-600 hover:underline">
+           ${job.resumeName}
+         </a>`
+      : "No Resume"
+  }
+</td>
+  <td class="px-6 py-4">
+    <span class="${
+      job.status === "Applied"
+        ? "bg-green-100 text-green-700"
+        : "bg-orange-100 text-orange-600"
+    } px-3 py-1 rounded-full text-sm font-medium">
+      ${job.status}
+    </span>
+  </td>
+  <td class="px-6 py-4">
+    <a
+      href="${job.jobLink}"
+      target="_blank"
+      class="text-blue-600 hover:underline"
+    >
+      Open
+    </a>
+  </td>
+  <td class="px-6 py-4">
+  <button onclick="viewJob(${index})"
+    class="text-blue-600 hover:underline">
+    View
+  </button>
+  <button onclick="editJob(${index})"
+    class="text-yellow-600 hover:underline">
+    Edit
+  </button>
+  <button onclick="toggleStatus(${index})"
+    class="text-green-600 hover:underline">
+    Status
+  </button>
+  <button onclick="deleteJob(${index})"
+    class="text-red-600 hover:underline">
+    Delete
+  </button>
+  </td>
+`;
+    jobTableBody.appendChild(row);
+  });
+}
+function updateDashboard() {
+  console.log("Dashboard Running");
+  const totalJobs = jobs.length;
+  const appliedJobs = jobs.filter((job) => job.status === "Applied").length;
+  const pendingJobs = jobs.filter((job) => job.status === "Pending").length;
+  totalJobsElement.textContent = totalJobs;
+  appliedJobsElement.textContent = appliedJobs;
+  pendingJobsElement.textContent = pendingJobs;
+}
+function viewJob(index) {
+  const job = jobs[index];
+  jobDetails.innerHTML = `
+    <p><strong>Company:</strong> ${job.company}</p>
+    <p><strong>Role:</strong> ${job.role}</p>
+    <p><strong>Added By:</strong> ${job.addedBy}</p>
+    <p><strong>Status:</strong> ${job.status}</p>
+    <p><strong>Resume:</strong> ${job.resumeName}</p>
+    <p>
+      <strong>Job Link:</strong>
+      <a
+        href="${job.jobLink}"
+        target="_blank"
+        class="text-blue-600 underline"
+      >
+        Open Job
+      </a>
+    </p>
+    <p>
+      <strong>Additional Info:</strong>
+      ${job.notes || "No Notes"}
+    </p>
+  `;
+  viewModal.classList.remove("hidden");
+  viewModal.classList.add("flex");
+}
+function saveToLocalStorage() {
+  localStorage.setItem("jobs", JSON.stringify(jobs));
+}
+function deleteJob(index) {
+  const confirmDelete = confirm("Are you sure you want to delete this job?");
+  if (!confirmDelete) return;
+  jobs.splice(index, 1);
+  renderJobs();
+  updateDashboard();
+  saveToLocalStorage();
+}
+function toggleStatus(index) {
+  const job = jobs[index];
+
+  job.status = job.status === "Pending" ? "Applied" : "Pending";
+
+  renderJobs();
+  updateDashboard();
+  saveToLocalStorage();
+}
+let editIndex = null;
+
+function editJob(index) {
+  const job = jobs[index];
+
+  companyInput.value = job.company;
+  roleInput.value = job.role;
+  jobLinkInput.value = job.jobLink;
+  addedByInput.value = job.addedBy;
+  statusInput.value = job.status;
+  notesInput.value = job.notes;
+
+  editIndex = index;
+
+  saveJobBtn.textContent = "Update Job";
+
+  jobModal.classList.remove("hidden");
+  jobModal.classList.add("flex");
+}
+renderJobs();
+updateDashboard();
+window.viewJob = viewJob;
+window.editJob = editJob;
+window.toggleStatus = toggleStatus;
+window.deleteJob = deleteJob;
---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCamR-1zz2m-Cmv6eZeid9go7z0zZ8YDQQ",
+  authDomain: "shared-job-tracker.firebaseapp.com",
+  projectId: "shared-job-tracker",
+  storageBucket: "shared-job-tracker.firebasestorage.app",
+  messagingSenderId: "648808614963",
+  appId: "1:648808614963:web:eb382bb3c36f17135d2746",
+  measurementId: "G-MZ7ZX3W82T",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc, getDocs };
